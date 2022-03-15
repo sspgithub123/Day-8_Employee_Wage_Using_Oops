f44bb2bcabf23837078725d6671f5b90eb8f56c9
Shubham <ssp671995@gmail.com>
2022-03-15 17:10:35 +0530

    Adding the wage for multiple companies.

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -16,6 +16,11 @@ public class EmployeeWageComputation {
         object.wageForMonth();
         object.conditionalWage();
         object.computeEmployeeWage();
+        System.out.println("WageForMultipleCompanies-8===============>>>>>>>>>>>>>>>>>>>>>");
+        calculateEmpWage("Jio", 20, 10, 15);
+        calculateEmpWage("Vodafone", 30, 8, 20);
+        calculateEmpWage("VI", 15, 4, 20);
+
     }
 
     public void attendance() {
@@ -161,6 +166,37 @@ public class EmployeeWageComputation {
             monthlyTotalWage = monthlyTotalWage + daily_wage_array[j];
         }
         System.out.println("\nMonthly wage is " + monthlyTotalWage);
+    }
+
+    //WageForMultipleCompanies
+    //Method to Calculate Employee Wage by Hours and Type
+    public static void calculateEmpWage(String company, int wagePerHr, int numWorkingDays, int maxHrPerMonth) {
+        // Local Variables
+        int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+        //Compute Emp Wage for Month
+        while (totalEmpHrs <= maxHrPerMonth && totalWorkingDays < numWorkingDays) {
+            totalWorkingDays++;     //Incrementing Working Days
+            double empCheck = Math.floor(Math.random() * 10) % 3;
+
+            switch ((int) empCheck) {
+                case IS_PART_TIME:
+                    System.out.println("Employee is Present as PART TIME");
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    System.out.println("Employee is Present as FULL TIME");
+                    empHrs = 8;
+                    break;
+                default:
+                    System.out.println("Employee is Absent");
+                    empHrs = 0;
+                    break;
+            }
+            totalEmpHrs += empHrs;
+            System.out.println("Day No :" + totalWorkingDays + " Emp Hrs: " + empHrs);
+        }
+        int totalEmpWage = totalEmpHrs * wagePerHr;
+        System.out.println("Total Employee Wage for Company : " + company + " is : " + totalEmpWage);
     }
 }
 
